a4b9fab6613aa875d94bb172302dff62a8a8873a
Daniel Debonzi <debonzi@geru.com.br>
2019-09-16 15:16:50 -0300

    Use RCA first connection timeout option.

--- crossover/xrequests.py
+++ crossover/xrequests.py
@@ -1,6 +1,13 @@
 # -*- encoding: utf-8 -*-
+import os
 import requests as _requests
 from rca.adapters import RedisCeleryAdapter
 
+TIMEOUT = os.environ.get('CROSSOVER_CONN_TIMEOUT')
+CROSSOVER_CONN_TIMEOUT = int(TIMEOUT) if TIMEOUT else None
+
 requests = _requests.Session()
-requests.mount('redis://', RedisCeleryAdapter())
+requests.mount(
+    'redis://',
+    RedisCeleryAdapter(first_connection_timeout=CROSSOVER_CONN_TIMEOUT)
+)
